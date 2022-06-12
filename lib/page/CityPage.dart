c11be97b4b6c6a47f48d0c271e817481bf5546fc
donghyun kim <imkim1893@naver.com>
2022-06-12 21:03:41 +0900

    FutureBuilder로 위젯 쉽게 작성하기

--- lib/page/CityPage.dart
+++ lib/page/CityPage.dart
@@ -7,12 +7,19 @@ class CityPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
+    return FutureBuilder<List<City>>(
       future: CityRepository.me.getList(),
       builder: (context, snapshot) {
         bool isDone = snapshot.connectionState==ConnectionState.done;
         if(!isDone) {
           return const Center(child: CircularProgressIndicator());
+        }
+
+        List<City> cityList = snapshot.data ?? [];
+        if(cityList.isEmpty) {
+          return Center(child: FloatingActionButton(onPressed: (){
+
+          }));
         }
 
         return Container(width: 100, height: 100, color: Colors.red);
