c72e66e12da91d0e724362c4dfdf6c1b61ff4f32
TryIt <imkim1893@naver.com>
2022-06-17 20:04:48 +0900

    defaultSection까지 완성

--- lib/page/MainPage.dart
+++ lib/page/MainPage.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../repository/PortpolioRepository.dart';
+import '../util/MyImage.dart';
+
 class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
+  List<Portpolio> portpolioList = [];
+  MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,10 +37,71 @@ class MainPage extends StatelessWidget {
   }
 
   Widget content() {
-    return Text("content", style: TextStyle(fontSize: 400));
+    return Column(
+      children: [
+        Text("트라잇 프로젝트"),
+        Text("트라잇은 고객이 원하는 프로덕트를 만듭니다."),
+        typeSelector(),
+        portpolioColumn(),
+      ],
+    );
   }
 
   Widget footer() {
     return Text("footer", style: TextStyle(fontSize: 400));
+  }
+
+  Widget typeSelector() {
+    return Row(
+      children:
+          ["전체", "프로덕트", "비즈니스", "블록체인", "아웃소싱"].map((e) => Text(e)).toList(),
+    );
+  }
+
+  Widget portpolioColumn() {
+    return Column(
+      children: portpolioList.map((e) => PortpolioWidget(e)).toList(),
+    );
+  }
+}
+
+class PortpolioWidget extends StatelessWidget {
+  double detailHeight = 0;
+  Portpolio portpolio;
+  PortpolioWidget(this.portpolio, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      defaultSection(),
+      detailSection(),
+    ]);
+  }
+
+  Widget defaultSection() {
+    return Row(
+      children: [
+        Image(image: MyImage.sampleImage),
+        Column(
+          children: [
+            Text(portpolio.type.toString()),
+            Text(portpolio.title.toString()),
+            Text(portpolio.content.toString()),
+            IconButton(
+              icon: Icon(Icons.arrow_downward),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget detailSection() {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 1000),
+      height: detailHeight,
+      child: Text("detailSection"),
+    );
   }
 }
---
+++ lib/util/MyImage.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class MyImage {
+  static const ImageProvider sampleImage = NetworkImage('https://lecle-s3-bucket.s3.ap-northeast-2.amazonaws.com/media/project/annot-thumbnail.png');
+}
+
